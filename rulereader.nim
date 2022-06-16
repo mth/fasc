4a72f06daa91e6f049b192ba1d2a87e7b526b2dc
mth <madis@cyber.ee>
2022-06-16 23:12:00 +0300

    ...

--- rulereader.nim
+++ rulereader.nim
@@ -4,10 +4,12 @@ import std/[strutils, parseutils]
 # https://nim-lang.org/docs/parseutils.html
 
 proc parseRule(src: string, start: var int): seq[string] =
-    let indent = src.skipWhitespace
+    let indent = src.skipWhitespace start
     start = start + indent
     if start >= src.len:
         return @[]
+    var word: string
+    let eow = str.parseUntil(word, {' ', '\t', '\f', '\r', '\n'}, start)
     return @[]
 
 echo readFile("README")
