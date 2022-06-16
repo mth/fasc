82faad2f576bce16e4e14be845d9b2fddb2d8d40
mth <madis@cyber.ee>
2022-06-17 01:59:41 +0300

    use inc

--- rulereader.nim
+++ rulereader.nim
@@ -8,14 +8,14 @@ const Space = {'\t', ' '}
 proc parseRule(src: string, start: var int): seq[string] =
     var indent = 0
     while start < src.len and src[start] in Whitespace:
-        start = start + src.skipWhile(Whitespace - Space, start)
+        start.inc src.skipWhile(Whitespace - Space, start)
         indent = src.skipWhile(Space, start)
-        start = start + indent
+        start.inc indent
     while start < src.len and not (src[start] in Newlines):
         var word: string
-        start = start + src.parseUntil(word, Whitespace, start)
+        start.inc src.parseUntil(word, Whitespace, start)
         result.add word
-        start = start + src.skipWhile(Whitespace - Newlines, start)
+        start.inc src.skipWhile(Whitespace - Newlines, start)
 
 let src = readFile("test.rule")
 var pos = 0
