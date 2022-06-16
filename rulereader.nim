154515e394aa63a9a29e51cb96923de7baecc6ee
mth <madis@cyber.ee>
2022-06-17 01:55:57 +0300

    use set ops

--- rulereader.nim
+++ rulereader.nim
@@ -3,17 +3,19 @@ import std/[strutils, parseutils]
 # https://nim-lang.org/docs/strutils.html
 # https://nim-lang.org/docs/parseutils.html
 
+const Space = {'\t', ' '}
+
 proc parseRule(src: string, start: var int): seq[string] =
     var indent = 0
     while start < src.len and src[start] in Whitespace:
-        start = start + src.skipWhile({'\n', '\v', '\f', '\r'}, start)
-        indent = src.skipWhile({'\t', ' '}, start)
+        start = start + src.skipWhile(Whitespace - Space, start)
+        indent = src.skipWhile(Space, start)
         start = start + indent
     while start < src.len and not (src[start] in Newlines):
         var word: string
         start = start + src.parseUntil(word, Whitespace, start)
         result.add word
-        start = start + src.skipWhile({' ', '\t', '\v', '\f'}, start)
+        start = start + src.skipWhile(Whitespace - Newlines, start)
 
 let src = readFile("test.rule")
 var pos = 0
