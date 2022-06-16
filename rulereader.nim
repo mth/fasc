dc12782d31cff6491e3fef32993bd1f40e68a535
mth <madis@cyber.ee>
2022-06-16 21:40:41 +0300

    ...

---
+++ rulereader.nim
@@ -0,0 +1,9 @@
+import std/[strutils, parseutils]
+
+# https://nim-lang.org/docs/strutils.html
+# https://nim-lang.org/docs/parseutils.html
+
+proc parseRule() =
+    return
+
+echo readFile("README")
