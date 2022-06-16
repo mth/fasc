1f762f17e7cc277932622491c889237e620e5819
mth <madis@cyber.ee>
2022-06-17 01:34:48 +0300

    parsing single rule

--- rulereader.nim
+++ rulereader.nim
@@ -4,12 +4,21 @@ import std/[strutils, parseutils]
 # https://nim-lang.org/docs/parseutils.html
 
 proc parseRule(src: string, start: var int): seq[string] =
-    let indent = src.skipWhitespace start
-    start = start + indent
-    if start >= src.len:
-        return @[]
-    var word: string
-    let eow = str.parseUntil(word, {' ', '\t', '\f', '\r', '\n'}, start)
-    return @[]
+    var indent = 0
+    while start < src.len and src[start] in Whitespace:
+        start = start + src.skipWhile({'\n', '\v', '\f', '\r'}, start)
+        indent = src.skipWhile({'\t', ' '}, start)
+        start = start + indent
+    while start < src.len and not (src[start] in Newlines):
+        var word: string
+        start = start + src.parseUntil(word, Whitespace, start)
+        if word == "":
+            break
+        result.add word
+        start = start + src.skipWhile({' ', '\t', '\v', '\f'}, start)
 
-echo readFile("README")
+let src = readFile("test.rule")
+var pos = 0
+while pos < src.len:
+    let cmd = src.parseRule pos
+    echo cmd
