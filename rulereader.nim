1276436ca3f0f704e4ae7145266fbe162f70bddc
mth <madis@cyber.ee>
2022-06-16 22:29:44 +0300

    ...

--- rulereader.nim
+++ rulereader.nim
@@ -3,7 +3,11 @@ import std/[strutils, parseutils]
 # https://nim-lang.org/docs/strutils.html
 # https://nim-lang.org/docs/parseutils.html
 
-proc parseRule() =
-    return
+proc parseRule(src: string, start: var int): seq[string] =
+    let indent = src.skipWhitespace
+    start = start + indent
+    if start >= src.len:
+        return @[]
+    return @[]
 
 echo readFile("README")
