3253a67e9c0da3230f9fe7e26022d10a1382f64b
mzz <mzz@linux.ee>
2022-07-20 06:53:08 +0300

    ...

--- testprops.nim
+++ testprops.nim
@@ -1,4 +1,4 @@
-import utils, std/[tables, os]
+import utils, std/[tables, os, sugar]
 
 var update: Table[string, proc(old: string): string]
 
@@ -6,9 +6,8 @@ for nth in 1 .. (paramCount() - 1) div 2:
   let idx = nth * 2
   let key = paramStr(idx)
   let value = paramStr(idx + 1)
-  echo("new: ", key, '=', value)
-  update[key] = proc(old: string): string =
-    echo("Old ", key, '=', old)
-    return value
+  capture value:
+    update[key] = proc(old: string): string =
+      return value
 
 modifyProperties(paramStr(1), update)
