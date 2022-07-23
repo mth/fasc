46da3abfbeaddde9a8dfe6da6421216d87c44ba9
mzz <mzz@linux.ee>
2022-07-24 01:20:07 +0300

    ...

--- shell.nim
+++ shell.nim
@@ -1,16 +1,7 @@
-const bashrc_prefix = """
-PATH="/home/mzz/bin:$PATH:/home/mzz/.opam/default/bin"
-"""
-
-# This part is unlikely to need customization
-const bashrc_fasc = """
-
-### BEGIN FASC injection
+# This part is unlikely to need customization, and probably should into /etc/bash.bashrc
+const bashrc = """
 # If not running interactively, don't do anything
-case $- in
-    *i*) ;;
-      *) return;;
-esac
+[ -z "$PS1" ] && return
 
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 export LESSCHARSET=utf-8
@@ -37,17 +28,20 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
-### END FASC injection
 
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
 """
 
-const bashrc_prefix = """
+const debian_bashrc_header = "# ~/.bashrc: executed by bash(1) for non-login shells."
+
+const dot_bashrc = """
+PATH="/home/mzz/bin:$PATH:/home/mzz/.opam/default/bin"
+[ -z "$PS1" ] && return
 PS1='\h \w\[\e`[ $? = 0 ]&&echo \e[32m||echo \e[31m`\]/\[\e[0m\] '
 export JAVA_HOME=/usr/lib/jvm/default-java
-alias ls='ls --color=auto'
-alias jgrep='rg -tjava'
-alias grep='grep --color=auto'
 alias less='less -cS'
+alias jgrep='rg -tjava'
 alias ocaml='rlwrap ocaml'
 alias screen='env LOCKPRG=/bin/true screen'
 alias bc='rlwrap bc'
