a60e2b9f4b554ab152c58abdc40f52cb7f1ab368
mzz <mzz@linux.ee>
2022-07-24 03:46:02 +0300

    ...

--- sway.nim
+++ sway.nim
@@ -86,8 +86,9 @@ bindsym $mod+l exec swaylock -f -c 000000
 bindsym $mod+s exec GRIM_DEFAULT_DIR=/tmp/downloads exec grim
 bindsym XF86MonBrightnessUp exec brightnessctl -q s +2%
 bindsym XF86MonBrightnessDown exec brightnessctl -q -n60 s 2%-
-bindsym XF86AudioRaiseVolume exec amixer sset Master 1+
-bindsym XF86AudioLowerVolume exec amixer sset Master 1-
+bindsym XF86AudioRaiseVolume exec amixer set Master 1+
+bindsym XF86AudioLowerVolume exec amixer set Master 1-
+bindsym XF86AudioMute exec amixer set Master toggle
 
 exec exec foot -s
 exec exec gammastep -m wayland
