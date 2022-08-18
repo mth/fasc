881cfb622dd133c381bac9404f5a8567c487da4a
mth <mzz@linux.ee>
2022-08-18 06:40:05 +0300

    vim

--- vim.nim
+++ vim.nim
@@ -1,7 +1,7 @@
 # TODO
 #  * Parse call plug#begin() ... call plug#end()
 #  * Changing/adding set foo= and PREFIX setlocal foo=
-#    (vim allows multiple assignments after set
+#    (vim allows multiple assignments after set)
 #  * Adding unique source directives
 #  * Write default colors and some other configurations
 # The init.vim parser should probably get list of handlers, and process input quite like modifyProperties
