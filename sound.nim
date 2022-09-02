13928a46243c3221c46e2a044cd69d27b09c94b8
Madis Janson <mzz@linux.ee>
2022-09-02 20:58:12 +0300

    ...

--- sound.nim
+++ sound.nim
@@ -7,3 +7,7 @@ proc configureALSA*(args: StrMap) =
   # CARD should be configurable by argument
   writeFile("/etc/asound.conf", [asoundrc.replace("CARD", "0")])
   packagesToInstall.add ["alsa-utils", "libasound2-plugins"]
+
+proc systemPulseaudio*(args: StrMap) =
+  packagesToInstall.add "pulseaudio"
+
