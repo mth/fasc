13d6b8e2c315420702e0fded9bb50358b5f79948
Madis Janson <mzz@linux.ee>
2022-09-04 03:43:58 +0300

    check WAYLAND_DISPLAY

--- resources/sandbox.sh
+++ resources/sandbox.sh
@@ -13,7 +13,14 @@ stop_zoom
 trap stop_zoom INT EXIT
 
 XDG_RUNTIME_DIR="/run/user/`id -u ${USER}`"
-chmod 770 "/run/user/$SUDO_UID/$WAYLAND_DISPLAY"
+BIND=""
+WENV=""
+if [ -n "$WAYLAND_DISPLAY" ]; then
+	chmod 770 "/run/user/$SUDO_UID/$WAYLAND_DISPLAY"
+	BIND="/run/user/$SUDO_UID/$WAYLAND_DISPLAY:$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY"
+	WENV="WAYLAND_DISPLAY=$WAYLAND_DISPLAY QT_QPA_PLATFORM=wayland-egl QT_WAYLAND_DISABLE_WINDOWDECORATION=1 XDG_SESSION_TYPE=wayland"
+fi
+
 /usr/bin/install -d -o ${USER} -g ${GROUP} -m 700 "$XDG_RUNTIME_DIR"
 systemd-run -PG --no-ask-password --service-type=exec --unit=${UNIT} \
 	-p ProtectSystem=strict \
@@ -24,6 +31,6 @@ systemd-run -PG --no-ask-password --service-type=exec --unit=${UNIT} \
 	-p CapabilityBoundingSet=~CAP_SYS_ADMIN \
 	-p 'ReadWritePaths=${HOME} /tmp' \
 	-p User=${USER} -p Group=${GROUP} \
-	-p "BindPaths=/run/user/$SUDO_UID/$WAYLAND_DISPLAY:$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" \
-	-p "Environment=HOME=${HOME} XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR DISPLAY=$DISPLAY WAYLAND_DISPLAY=$WAYLAND_DISPLAY QT_QPA_PLATFORM=wayland-egl QT_WAYLAND_DISABLE_WINDOWDECORATION=1 XDG_SESSION_TYPE=wayland" \
+	-p "BindPaths=$BIND" \
+	-p "Environment=HOME=${HOME} XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR DISPLAY=$DISPLAY $WENV" \
 	${COMMAND} "$@"
