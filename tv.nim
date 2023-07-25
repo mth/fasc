1528732e02b8a1aa88a75a12e5abb11aca2fb855
mth <mth@localhost>
2023-07-25 04:25:45 +0300

    ...

--- tv.nim
+++ tv.nim
@@ -13,6 +13,6 @@ proc westonTV*(args: StrMap) =
   user.runWayland "/usr/bin/weston"
   packagesToInstall.add ["weston", "openssh-client", "foot", "celluloid", "mpv",
                          "sonata", "geeqie", "fonts-terminus-otb", "mpd"]
-  user.installMpd
+  user.installMpd # commits queue
   for (path, content, mode) in util_files:
     writeFile path, [content], false, mode.Mode
