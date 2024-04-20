2bef5afd1b2069d9e128762d93009b38e7a750f9
mth <mth@localhost>
2024-04-21 00:03:22 +0300

    ...

--- opam.nim
+++ opam.nim
@@ -3,6 +3,7 @@ import utils
 const emacs = readResource("emacs")
 
 proc installOpam() =
+  # it would be nice to detect whether sway or X11 is used (check for wayland socket?)
   packagesToInstall.add ["opam", "elpa-company", "elpa-tuareg"]
   # add .emacs
   # as user run
