7bf5aa03b199e7508636375169f5cd2c4dca2832
mth <mth@localhost>
2024-05-15 18:18:27 +0300

    ...

--- network.nim
+++ network.nim
@@ -179,8 +179,7 @@ proc ovpnClient*(args: StrMap) =
     commitQueue()
   else:
     user.sudoNoPasswd "", ovpnPath, killVPNPath
-  runCmd("systemctl", "disable", "openvpn")
-  runCmd("systemctl", "stop", "openvpn")
+  runCmd "systemctl", "disable", "--now", "openvpn"
   if not fileExists("/root/.vpn/client.ovpn"):
     createDir("/root/.vpn")
     setPermissions("/root/.vpn", 0o700)
