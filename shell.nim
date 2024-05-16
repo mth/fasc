f0ebcb58a07790bb8bd6da9c6bcad98be62765d4
mth <mth@localhost>
2024-05-16 14:08:08 +0300

    fedora bash

--- shell.nim
+++ shell.nim
@@ -5,6 +5,7 @@ import utils
 const etc_bashrc = readResource("bash.bashrc")
 const dot_bashrc = readResource("user/bashrc")
 const debian_bashrc_header = "# ~/.bashrc: executed by bash(1) for non-login shells."
+const fedora_bashrc_header = "# .bashrc"
 const upload_cam_script = readResource("user/upload-cam")
 const upload_cam_desktop = readResource("user/upload-cam.desktop")
 const helix = readResource("user/helix.toml")
@@ -12,13 +13,18 @@ const helix = readResource("user/helix.toml")
 proc configureUserBash(user: UserInfo) =
   let bashrc = user.home / ".bashrc"
   try:
-    if readLines(bashrc, 1) != [debian_bashrc_header]:
-      echo(bashrc, " is not a debian default, not modifying")
+    let bashrc_first = readLines(bashrc, 1)
+    if bashrc_first != [debian_bashrc_header] and
+       bashrc_first != [fedora_bashrc_header]:
+      echo bashrc, " is not a debian default, not modifying"
       return
   except:
     discard # non-existent .bashrc isn't a problem
   echo("Replacing ", bashrc)
-  writeFile(bashrc, dot_bashrc)
+  if isDebian():
+    writeFile(bashrc, dot_bashrc)
+  else:
+    writeFile(bashrc, dot_bashrc.replace("&& return\n", "&& return\n. /etc/bash.bashrc\n"))
   user.writeAsUser(".config/helix/config.toml", helix)
 
 proc configureBash*(args: StrMap) =
