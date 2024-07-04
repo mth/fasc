1b7a76775e8bc18807bc1a23dc77cc715f0066f3
mth <mth@localhost>
2024-07-05 01:59:45 +0300

    ...

--- utils.nim
+++ utils.nim
@@ -364,5 +364,5 @@ proc addSystemUser*(user, group, home: string) =
 
 proc sparseFile*(filename: string, size: Off) =
   if filename.truncate(size) != 0:
-    raise newException(OSError, $strerror(errno))
+    raise newException(OSError, fmt"truncate({filename}): {$strerror(errno)}")
 
