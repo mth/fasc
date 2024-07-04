9a7bf34db844bace2c0de2bc53317b0d0338a3d0
mth <mth@localhost>
2024-07-05 00:37:05 +0300

    onDemandMount

--- backup.nim
+++ backup.nim
@@ -1,8 +1,10 @@
 #import std/[parseutils, sequtils, strformat, strutils, os, tables]
 #import apps, services, utils
 
-import std/[strformat, os]
+import std/[strformat, strutils, os]
 import utils
+
+const backupMountPoint = "/media/backupstore"
 
 func sshUserConfig(user, chrootDir: string): string = fmt"""
 Match User {user}
@@ -31,16 +33,36 @@ proc createBackupUser(name, home: string): UserInfo =
     addSystemUser name, group, home
     return name.userInfo
 
+func mountUnit(description, unit, what, where: string) = fmt"""
+[Unit]
+Description={description}
+{unit}
+[Mount]
+What={what}
+Where={where}
+"""
+
+proc onDemandMount(description, dev, mount: string): string =
+  var what = dev
+  if what.startsWith('/'):
+    what = outputOfCommand("", "blkid", "-o", "value", "-s", "UUID", dev).strip
+  var unit = mountUnit(description, "StopWhenUnneeded=true\n", dev, mount)
+  unit &= "Options=noatime,noexec,nodev,noauto\n"
+  result = mount.strip({'/'}).replace('/', '-') & ".mount" 
+  writeFile "/etc/systemd/system/" & result, [unit], true
+
+proc backupMount(dev: string): string =
+  onDemandMount "Backup store mount", dev, backupMountPoint
 
 # TODO server
 # * socket-activation vahendaja, et nbd-server käivitada ainult vastavalt vajadusele 
 #   (using proxy function from services module)
 # * nbd-server systemd teenus
-# * on-demand systemd mount backup failisüsteemile, mida nbd-server kasutaks
+# % on-demand systemd mount backup failisüsteemile, mida nbd-server kasutaks
 #   + Determine UUID by device path
 #     blkid -o value -s UUID /dev/sda2
-# * kasutaja loomine kes saaks ssh kaudu seda soklit kasutada
-# * sshd conf ChrootDirectory ja AllowStreamLocalForwarding local kasutajale
+# % kasutaja loomine kes saaks ssh kaudu seda soklit kasutada
+# % sshd conf ChrootDirectory ja AllowStreamLocalForwarding local kasutajale
 # * script to rotate backup images
 
 # TODO klient
