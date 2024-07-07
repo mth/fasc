5a83ddebe2cd48997940572c805106701cc905c8
mth <mth@localhost>
2024-07-07 19:45:40 +0300

    ...

--- resources/rotate-backup.sh
+++ resources/rotate-backup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-BACKUPS_DIR="/media/backupstore/client"
-
-for CLIEND_DIR in $BACKUPS_DIR/*; do
+for CLIENT_DIR in /media/backupstore/client/*; do
 	ACTIVE_IMAGE="$CLIENT_DIR/active/backup.img"
 	if [ -e "$ACTIVE_IMAGE" ]; then
-		cp --reflink "$ACTIVE_IMAGE" "$CLIENT_DIR/backup-TODO.img"
+		NUMBER="$(("$(/bin/date +%j)" / 28 % 3))"
+		/bin/cp --reflink "$ACTIVE_IMAGE" "$CLIENT_DIR/backup-$NUMBER.img"
 	fi
 done
