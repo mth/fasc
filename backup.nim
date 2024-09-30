3bee613b2d19108cf0b6f4a8fce11aac8475a1d2
mth <mth@localhost>
2024-09-30 13:25:12 +0300

    ...

--- backup.nim
+++ backup.nim
@@ -190,7 +190,7 @@ proc installBackupClient*(args: StrMap) =
   addService "nbd-backup", "Start NBD backup client", [],
              "systemd-inhibit --what=idle:sleep:handle-suspend-key:handle-hibernate-key --who=nbd-backup \"--why=Active backup\" /usr/local/sbin/nbd-backup sync",
              options=["ExecStartPre=" & waitWakeup, "User=root", "PAMName=crond"],
-             unitOptions=["ConditionACPower=true"]
+             unitOptions=["ConditionACPower=true", "BindsTo=user@0.service", "After=user@0.service"]
   addTimer "nbd-backup", "Starts NBD backup client periodically",
            ["OnCalendar=*-*-02/4 05:05:05", "WakeSystem=true"]
   let sshConfig = "/root/.ssh/config"
