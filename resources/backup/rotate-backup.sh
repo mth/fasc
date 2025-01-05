57bc1640ae50d4365ec43796708915fb2cdf1fb5
mth <mzz@linux.ee>
2025-01-05 14:20:33 +0200

    fix rotation

--- resources/backup/rotate-backup.sh
+++ resources/backup/rotate-backup.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 for CLIENT_DIR in /media/backupstore/client/*; do
-	ACTIVE_IMAGE="$CLIENT_DIR/active/backup.img"
+	ACTIVE_IMAGE="$CLIENT_DIR/active/backup.image"
 	if [ -e "$ACTIVE_IMAGE" ]; then
+		# % is bash
 		NUMBER="$(("$(/bin/date +%m)" % 3))"
-		/bin/cp --reflink "$ACTIVE_IMAGE" "$CLIENT_DIR/backup-$NUMBER.img"
+		/bin/cp -v --reflink "$ACTIVE_IMAGE" "$CLIENT_DIR/backup-$NUMBER.image" || true
 	fi
 done
