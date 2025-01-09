9f11109c3624c506e139d4a4c4f8a08f61c58dd1
mth <mth@localhost>
2025-01-09 16:23:57 +0200

    Use private_all in proxy

--- services.nim
+++ services.nim
@@ -175,14 +175,14 @@ proc proxy*(proxy, listen, bindTo, connectTo, exitIdleTime, targetService: strin
   if socketParam.len > 3:
     socket &= fmt"SocketMode={socketParam[3]}"
   var options = @["PrivateTmp=yes"]
+  var flags = {s_sandbox, s_private_dev, s_call_filter, s_overwrite}
   if listen.startsWith("/") and connectTo.startsWith("/"):
-    options.add "PrivateNetwork=yes"
+    flags.incl s_private_all
   let requires = if targetService != "": @[targetService, socketName]
                  else: @[socketName]
   addService(socketParam[0], descriptionStr, requires,
     "/usr/lib/systemd/systemd-socket-proxyd --exit-idle-time=" &
-    exitIdleTime & ' ' & connectTo, "",
-    {s_sandbox, s_private_dev, s_call_filter, s_overwrite}, options)
+    exitIdleTime & ' ' & connectTo, "", flags, options)
   socketUnit socketName, descriptionStr, listen, socket
 
 proc socketProxy*(args: StrMap) =
